56b6810ca5e006c45eaeb3e65e585bd9dd56a079
Habibullah800 <mdhabibullah800@gmail.com>
2022-08-23 01:35:03 +0600

    Hero section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Head/Header';
+import Home from './Components/Hero/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
     </div>
   );
 }
